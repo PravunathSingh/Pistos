a9fffdc3b7cd2ee41a98f7e519aecf8db992fa0a
Pravunath Singh <pravunathsingh@gmail.com>
2022-03-03 16:26:58 +0530

    added payment gateway (on the frontend)

--- src/components/checkout/CheckoutBody.js
+++ src/components/checkout/CheckoutBody.js
@@ -1,7 +1,16 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Cart } from '../../context/cartContext';
 
 const CheckoutBody = () => {
+  const [checkoutForm, setCheckoutForm] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    address: '',
+    country: '',
+    pin: '',
+  });
+
   const cartCtx = useContext(Cart);
   const cartLength = cartCtx.cartLength;
 
@@ -15,11 +24,53 @@ const CheckoutBody = () => {
   const initialBill = 0;
   const totalBill = itemArray.reduce((acc, item) => acc + item, initialBill);
 
+  const handleFormChange = (e) => {
+    e.persist();
+    setCheckoutForm({ ...checkoutForm, [e.target.name]: e.target.value });
+  };
+
+  const submitOrder = (e) => {
+    e.preventDefault();
+    var data = {
+      name: checkoutForm.name,
+      phone: checkoutForm.phone,
+      email: checkoutForm.email,
+      address: checkoutForm.address,
+      country: checkoutForm.country,
+      pin: checkoutForm.pin,
+    };
+
+    var options = {
+      key: 'rzp_test_UrYqb5LGWAWmJT', // Enter the Key ID generated from the Dashboard
+      amount: totalBill * 100,
+      name: 'Eatiano',
+      description: 'Test Transaction',
+      // order_id: 'order_9A33XWu170gUtm', //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+      handler: function (response) {
+        alert(response.razorpay_payment_id);
+      },
+      prefill: {
+        name: data.name,
+        email: data.email,
+        contact: data.phone,
+      },
+      notes: {
+        address: data.address,
+      },
+      theme: {
+        color: '#2F343A',
+      },
+    };
+
+    const razorPay = new window.Razorpay(options);
+    razorPay.open();
+  };
+
   return (
     <div className='container grid gap-24 my-16 md:my-24 lg:my-40 md:grid-cols-2'>
       <div className='order-2 md:order-1'>
         <div>
-          <form>
+          <form onSubmit={submitOrder}>
             <div className='mb-6 md:mb-9'>
               <label>
                 <h6 className='mb-4 text-lg font-medium text-gray-100 lg:text-xl md:mb-6'>
@@ -27,8 +78,12 @@ const CheckoutBody = () => {
                 </h6>
               </label>
               <input
+                name='name'
                 type='text'
                 className='w-full px-4 py-2 text-gray-200 rounded-lg outline-none lg:text-lg focus:ring-2 ring-offset-2 ring-secondary bg-secondary'
+                required
+                onChange={handleFormChange}
+                value={checkoutForm.name}
               />
             </div>
 
@@ -39,8 +94,12 @@ const CheckoutBody = () => {
                 </h6>
               </label>
               <input
+                name='email'
                 type='email'
                 className='w-full px-4 py-2 text-gray-200 rounded-lg outline-none lg:text-lg focus:ring-2 ring-offset-2 ring-secondary bg-secondary'
+                required
+                onChange={handleFormChange}
+                value={checkoutForm.email}
               />
             </div>
 
@@ -51,8 +110,12 @@ const CheckoutBody = () => {
                 </h6>
               </label>
               <input
+                name='phone'
                 type='number'
                 className='w-full px-4 py-2 text-gray-200 rounded-lg outline-none lg:text-lg focus:ring-2 ring-offset-2 ring-secondary bg-secondary'
+                required
+                onChange={handleFormChange}
+                value={checkoutForm.phone}
               />
             </div>
 
@@ -63,9 +126,13 @@ const CheckoutBody = () => {
                 </h6>
               </label>
               <textarea
+                name='address'
                 cols='30'
                 rows='3'
                 className='w-full px-4 py-2 text-gray-200 rounded-lg outline-none lg:text-lg focus:ring-2 ring-offset-2 ring-secondary bg-secondary'
+                required
+                onChange={handleFormChange}
+                value={checkoutForm.address}
               ></textarea>
             </div>
 
@@ -77,8 +144,12 @@ const CheckoutBody = () => {
                   </h6>
                 </label>
                 <input
+                  name='country'
                   type='text'
                   className='w-full px-4 py-2 text-gray-200 rounded-lg outline-none lg:text-lg focus:ring-2 ring-offset-2 ring-secondary bg-secondary'
+                  required
+                  onChange={handleFormChange}
+                  value={checkoutForm.country}
                 />
               </div>
 
@@ -89,8 +160,12 @@ const CheckoutBody = () => {
                   </h6>
                 </label>
                 <input
+                  name='pin'
                   type='number'
                   className='w-full px-4 py-2 text-gray-200 rounded-lg outline-none lg:text-lg focus:ring-2 ring-offset-2 ring-secondary bg-secondary'
+                  required
+                  onChange={handleFormChange}
+                  value={checkoutForm.pin}
                 />
               </div>
             </div>
@@ -98,9 +173,6 @@ const CheckoutBody = () => {
             {/* <div className='flex flex-wrap items-center justify-end gap-4'>
               
             </div> */}
-            <button className='w-full px-8 py-3 mb-6 mr-10 text-lg font-medium text-gray-900 transition-all duration-300 rounded-lg hover:text xl:w-auto bg-cta md:text-xl hover:bg-cta-dark hover:scale-110 focus:ring-2 ring-offset-2 ring-cta-dark xl:mb-0'>
-              Pay With PayPal
-            </button>
             <button className='w-full px-8 py-3 text-lg font-medium text-gray-900 transition-all duration-300 rounded-lg hover:text xl:w-auto bg-cta md:text-xl hover:bg-cta-dark hover:scale-110 focus:ring-2 ring-offset-2 ring-cta-dark'>
               Pay With RazorPay
             </button>
